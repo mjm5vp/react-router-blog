a2aa419a8403744348d8b6753fe103b14d9a83e0
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-10-08 16:12:15 -0400

    posts show and delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_POSTS } from './types';
+import { FETCH_POSTS, CREATE_POST, FETCH_POST, DELETE_POST } from './types';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=MARKYMARKMARK';
@@ -9,5 +9,35 @@ export function fetchPosts() {
   return {
     type: FETCH_POSTS,
     payload: request
+  };
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts/${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}/${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
